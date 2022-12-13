c38a552c8f4d5b31a63f511cb677f95aa433fcde
firs-iln <firs_iln@mail.ru>
2022-12-13 03:32:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from resources import Product, Products, Model, Price
 
-
 app = Flask(__name__)
 api = Api(app)
 
@@ -13,5 +12,4 @@ api.add_resource(Products, api_base_url + "products/")
 api.add_resource(Model, api_base_url + "model/<int:product_id>")
 api.add_resource(Price, api_base_url + "price/<int:product_id>")
 
-if __name__ == "__main__":
-    app.run()
+
